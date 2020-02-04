e10f8b6fda0af6303da7b76b6b7a3eb2d177191f
afondevilarevelli <antonny.42@gmail.com>
2020-02-04 15:22:25 -0300

    Advance with home posts page

--- main_menu/views.py
+++ main_menu/views.py
@@ -3,8 +3,11 @@ from db.models import Post
 
 def index(req):
     if req.COOKIES.get('user_cookie'):
+        #modelView = {
+        #    "posts": Post.objects.filter(owner=req.COOKIES.get('user_cookie')).order_by('date')
+        #}
         modelView = {
-            "posts": Post.objects.filter(owner=req.COOKIES.get('user_cookie')).order_by('date')
+            "posts": [Post(text="alto post")]
         }
         return render(req, 'main_menu/index.html', context=modelView)
     else:
